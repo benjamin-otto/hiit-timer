4b60fd49fb8bef526ce6ee6192ad29440ecc50f8
benjamin-otto <benjaminotto@protonmail.ch>
2019-10-06 15:52:30 +0200

    Reduced pad left to 1 digit.

--- app/utils/utils.js
+++ app/utils/utils.js
@@ -2,6 +2,6 @@
 export function secondsToMinutes(seconds) {
   const mins = Math.floor(seconds / 60)
   const secs = seconds - (mins * 60)
-
-  return `${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`
+  // return `${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`
+  return `${mins}:${secs.toString().padStart(2, '0')}`
 }
