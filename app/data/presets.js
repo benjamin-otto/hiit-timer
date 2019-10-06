295a5169322320d3ac6b5536ec0732668a92eddb
benjamin-otto <benjaminotto@protonmail.ch>
2019-10-06 15:51:33 +0200

    Added presets data.

---
+++ app/data/presets.js
@@ -0,0 +1,38 @@
+const presets = [{
+  title: 'Tabata',
+  warmup: 120,
+  low: 10,
+  high: 20,
+  cooldown: 120,
+  rounds: 8
+}, {
+  title: '30 Split',
+  warmup: 120,
+  low: 30,
+  high: 30,
+  cooldown: 120,
+  rounds: 10
+}, {
+  title: 'Boxing',
+  warmup: 120,
+  low: 60,
+  high: 180,
+  cooldown: 120,
+  rounds: 10
+}, {
+  title: 'No Warmup',
+  warmup: 0,
+  low: 30,
+  high: 30,
+  cooldown: 120,
+  rounds: 10
+}, {
+  title: 'Super Short',
+  warmup: 2,
+  low: 2,
+  high: 2,
+  cooldown: 2,
+  rounds: 1
+}]
+
+export default presets
