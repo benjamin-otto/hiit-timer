5c6ce643b4e64ed6b776fa50c6b544d76e143d40
benjamin-otto <benjaminotto@protonmail.ch>
2019-10-06 15:46:31 +0200

    Added Timer component and audio.

--- app/index.js
+++ app/index.js
@@ -4,26 +4,30 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './css/base.css'
 import './css/light.css'
 import './css/dark.css'
+import './css/timer.css'
 
 import { ThemeProvider } from './contexts/theme'
 import Nav from './components/Nav'
 import Presets from './components/Presets'
 import Custom from './components/Custom'
+import Timer from './components/Timer'
 
 function App() {
   const [theme, setTheme] = React.useState('light')
+  const [playAudio, setPlayAudio] = React.useState(true)
   const toggleTheme = () => { setTheme((theme) => theme === 'light' ? 'dark' : 'light') }
+  const togglePlayAudio = () => { setPlayAudio((playAudio) => playAudio === true ? false : true) }
 
   return (
     <Router>
       <ThemeProvider value={theme}>
         <div className={`container ${theme}`}>
           <div className='inner-box'>
-            <Nav toggleTheme={toggleTheme} />
-
+            <Nav toggleTheme={toggleTheme} playAudio={playAudio} togglePlayAudio={togglePlayAudio} />
             <Switch>
               <Route path='/' exact component={Presets} />
               <Route path='/custom' component={Custom} />
+              <Route path='/timer' render={(props) => <Timer {...props} playAudio={playAudio} />} />
               <Route render={() => <h1>404</h1>} />
             </Switch>
           </div>
